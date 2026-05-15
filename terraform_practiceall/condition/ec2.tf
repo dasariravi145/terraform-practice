a6c7570cdbe41ef99976db3483d2ee716a11f790
unknown <dasariravid145@gmail.com>
2026-05-15 11:44:39 +0530

    action

---
+++ terraform_practiceall/condition/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "example"{
+
+     ami = var.ami
+     instance_type = var.env == "dev" ? "t3.micro" : "t3.small"
+     
+     vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+     tags = var.tags
+}
+
+resource "aws_security_group" "allow_tls"{
+
+       name = var.sg_name
+       description = var.sg_description
+
+       ingress {
+
+            from_port        = var.from_port
+            to_port          = var.to_port
+            protocol         = "-1"
+            cidr_blocks      = var.cidr_blocks
+            ipv6_cidr_blocks = var.ipv6_cidr_blocks
+
+       }
+       egress {
+
+            from_port        = var.from_port
+            to_port          = var.to_port
+            protocol         = "-1"
+            cidr_blocks      = var.cidr_blocks
+            ipv6_cidr_blocks = var.ipv6_cidr_blocks
+       }
+
+    tags = var.sg_tags
+}
