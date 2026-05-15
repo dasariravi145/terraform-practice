df9e7f9814415b669de2be054a85bdf1575b916e
unknown <dasariravid145@gmail.com>
2026-05-15 18:02:59 +0530

    action

--- terraform_practiceall/count/ec2.tf
+++ terraform_practiceall/count/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example"{
 
-     count = 10
+     count = length(var.tags)
      ami = var.ami
      instance_type = var.instance_type
      vpc_security_group_ids = [aws_security_group.allow_tls.id]
