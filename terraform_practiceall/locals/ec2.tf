b8f6a31aac4837ee5eeb7c5ea43c09dbe96b165d
unknown <dasariravid145@gmail.com>
2026-05-16 12:57:55 +0530

    action

---
+++ terraform_practiceall/locals/ec2.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "example" {
+
+       ami = "ami-0220d79f3f480ecf5"
+       instance_type = "t3.micro"
+
+    tags = {
+       Name = "terraform-state"
+       project ="roboshop"
+  }
+}
+
+resource  "aws_security_group" "allow_tls" {
+
+      name = "allow-all-roboshop"
+      description = "Allow TLS inbound traffic and all outbound traffic"
+
+      ingress {
+
+            from_port        = 0
+            to_port          = 0
+            protocol         = "-1"
+            cidr_blocks      = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = ["::/0"]
+      }
+      egress {
+            from_port        = 0
+            to_port          = 0
+            protocol         = "-1"
+            cidr_blocks      = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = ["::/0"]
+   }
+
+   tags = {
+    Name = "allow-all-terraform"
+  }
+
+} 
