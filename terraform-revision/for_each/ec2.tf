6793d80e73766e3d41a042c873c8e0c12d3bcfc9
unknown <dasariravid145@gmail.com>
2026-06-21 09:59:55 +0530

    terraform

--- terraform-revision/for_each/ec2.tf
+++ terraform-revision/for_each/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-         for_each = lenght(var.instance_type)
+         for_each = var.instance_type
          ami = "ami-0220d79f3f480ecf5"
          instance_type = each.value
          vpc_security_group_ids = [aws_security_group.allow_tls.id]
--- terraform-revision/for_each/variable.tf
+++ terraform-revision/for_each/variable.tf
@@ -9,3 +9,14 @@ variable "instance_type" {
             payment = "t3.large"
        }
 }
+
+variable "zone_id" {
+
+    default = "Z05454352LP47ZQWSG7WJ"
+}
+
+variable "domain_name" {
+
+     default = "dsecops88.online"
+}
+
