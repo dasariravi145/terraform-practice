07fcb5c1b56a4e2d6f9ea1e5ef90b7c9aca56e59
unknown <dasariravid145@gmail.com>
2026-06-21 09:57:19 +0530

    terraform

--- terraform-revision/count/variable.tf
+++ terraform-revision/count/variable.tf
@@ -2,3 +2,22 @@ variable "tag_name" {
     type = list(string)
     default = ["fronted","cart","user","mongodb"]      
 }
+
+variable "fruits" {
+
+      type = list(string)
+      default =["apple","banana","apple","graps"]
+
+}
+
+variable "zone_id" {
+
+      type = string
+     default = "Z05454352LP47ZQWSG7WJ"
+}
+
+variable "domian_name" {
+      
+      type = string
+      default = "dsecops88.online"
+}
