7fac031af3ce8af17fc5f860a24f4c5836735971
unknown <dasariravid145@gmail.com>
2026-06-24 17:26:40 +0530

    terraform

--- terraform-revision/terraform-aws-vpc/variable.tf
+++ terraform-revision/terraform-aws-vpc/variable.tf
@@ -35,3 +35,56 @@ variable "public_subnet_tags"{
      type = map
      default = {}
 }
+
+variable "private_subnet_cidrs" {
+
+       type = list
+       default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+
+variable "private_subnet_tags"{
+     type = map
+     default = {}
+}
+
+variable "database_subnet_cidrs" {
+
+       type = list
+       default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+variable "database_subnet_tags"{
+     type = map
+     default = {}
+}
+
+variable "public_route_table_tags" {
+     type = map
+     default= {}
+}
+
+variable "private_route_table_tags" {
+     type = map
+     default= {}
+}
+
+variable "database_route_table_tags" {
+     type = map
+     default= {}
+}
+
+variable "eip_tags" {
+      type = map
+      default = {}
+}
+
+variable "nat_gateway_tags" {
+     type = map
+     default = {}
+}
+
+variable "is_peering_required" {
+
+     default = false
+     type = bool
+}
