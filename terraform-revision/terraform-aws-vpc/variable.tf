cd6504c8bdd42ae86b11203eb1d2bd326184ac67
unknown <dasariravid145@gmail.com>
2026-06-24 14:22:02 +0530

    terraform

--- terraform-revision/terraform-aws-vpc/variable.tf
+++ terraform-revision/terraform-aws-vpc/variable.tf
@@ -18,3 +18,20 @@ variable "vpc_tags" {
      type = map
      default = {}
 }
+
+variable "igw_tags" {
+
+      type = map
+      default = {}
+}
+
+variable "public_subnet_cidrs" {
+
+       type = list
+       default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+variable "public_subnet_tags"{
+     type = map
+     default = {}
+}
