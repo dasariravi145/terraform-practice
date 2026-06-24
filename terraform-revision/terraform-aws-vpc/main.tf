48201bc4ea9183d1ca5dc35da3b9cc86d45f3787
unknown <dasariravid145@gmail.com>
2026-06-24 14:21:33 +0530

    terraform

--- terraform-revision/terraform-aws-vpc/main.tf
+++ terraform-revision/terraform-aws-vpc/main.tf
@@ -3,10 +3,29 @@ resource "aws_vpc" "main" {
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
-  tags = merge(
-    var.common_tags,
-    {
-        Name = "{var.project}-{var.environment}"
-    }),
-    var.vpc_tags
+  tags = local.vpc_aws_finaltags
+}
+
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = local.igw_final_tags
+}
+
+resource "aws_subnet" "public" {
+    count = length(var.public_subnet_cidrs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.public_subnet_cidrs[count.index]
+    availability_zone = local.az_names[count.index]
+    map_public_ip_on_launch = true
+
+    tags = merge(
+         local.common_tags,
+         {
+
+              Name= "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+         },
+         var.public_subnet_tags
+    )
 }
