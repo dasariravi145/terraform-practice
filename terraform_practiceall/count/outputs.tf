cf29e2935cc4976ee165b69913504f0ac1b79be4
unknown <dasariravid145@gmail.com>
2026-07-08 17:04:34 +0530

    terraform

---
+++ terraform_practiceall/count/outputs.tf
@@ -0,0 +1,13 @@
+#output "roboshop_instance"{
+    # value = aws_instance.example
+    # description= "description"
+#}
+
+output "fruits"{
+    value = var.fruits
+}
+
+output "fruits_set"{
+
+     value = var.fruits_set
+}
