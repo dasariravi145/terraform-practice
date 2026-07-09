ee409acb7d24c18e5e75a76834da6d40599d4a4a
unknown <dasariravid145@gmail.com>
2026-07-09 09:45:17 +0530

    terraform

---
+++ terraform_08_revision/condition/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "main" {
+
+         ami = var.ami
+         instance_type = var.env = "dev" ? "t3.micro" : "t3.large"
+         vpc_security_group_ids = [aws_security_group.allow_tls.id]
+        
+    tags = var.tags
+}
+
+resource "aws_security_group" "allow_tls" {
+
+          name        = var.sg_name
+          description = var.sg_description
+
+      egress {
+            from_port        = var.from_port
+            to_port          = var.to_port
+            protocol         = var.protocol
+            cidr_blocks      = var.cidr_blocks
+            ipv6_cidr_blocks = var.ipv6_cidr_blocks
+    }
+   
+    ingress {
+            from_port        = var.from_port
+            to_port          = var.to_port
+            protocol         = var.protocol
+            cidr_blocks      = var.cidr_blocks
+            ipv6_cidr_blocks = var.ipv6_cidr_blocks
+    }
+   tags = var.sg_tags
+}
---
+++ terraform_08_revision/condition/variable.tf
@@ -0,0 +1,79 @@
+variable "ami" {
+
+      type = string
+      default = "ami-0220d79f3f480ecf5"
+}
+
+variable "env" {
+      type = string
+      default = "dev"
+}
+
+#variable "instance_type" {
+
+ #     type = string
+  #    default = "t3.micro"
+#}
+
+variable "tags" {
+
+        type = map(string)
+
+        default = {
+
+              Name = "terraform"
+              Project = "roboshop"
+              Terraform = "true"
+              Environment = "dev"
+        }
+}
+
+variable "sg_name" {
+
+       type = string 
+       default = "allow-all-terraform"
+}
+
+variable "sg_description" {
+
+         type = string
+         default = "Allow TLS inbound traffic and all outbound traffic"      
+}
+
+variable "from_port" {
+      type = number
+      default = 0 
+} 
+
+variable "to_port" {
+      type = number
+      default = 0
+} 
+
+variable "protocol" {
+      type = string
+      default = "-1"
+}
+
+variable "cidr_blocks" {
+      type = list(string)
+      default = ["0.0.0.0/0"]
+}
+
+variable "ipv6_cidr_blocks" {
+      type = list(string)
+      default = ["::/0"]
+}
+
+variable "sg_tags" {
+
+       type = map
+
+       default = {
+
+              Name = "terraform"
+              Project = "roboshop"
+              Terraform = "true"
+              Environment = "dev"
+       }
+}
