dbdc07790f352f482fc9f3fb228a23a84bb815da
unknown <dasariravid145@gmail.com>
2026-07-09 14:51:58 +0530

    terraform

--- terraform_08_revision/dynamic/variable.tf
+++ terraform_08_revision/dynamic/variable.tf
@@ -7,13 +7,13 @@ variable "ingress" {
                    cidr_blocks = ["0.0.0.0/0"]
                    description = "allowing port number 22 from internet"
 
-              }
+              },
               {
                    port = 443
                    cidr_blocks = ["0.0.0.0/0"]
                    description = "allowing port number 443 from internet"
 
-              }
+              },
               {
                    port = 3306
                    cidr_blocks = ["0.0.0.0/0"]
