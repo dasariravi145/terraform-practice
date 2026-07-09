93c49a2f55085b510b361c0787b825d513e216c9
unknown <dasariravid145@gmail.com>
2026-07-09 11:20:51 +0530

    terraform

--- terraform_08_revision/count/variable.tf
+++ terraform_08_revision/count/variable.tf
@@ -3,3 +3,24 @@ variable "instances" {
       type = list(string)
       default = ["mongodb","user","redis","rabbitmq"]
 }
+
+variable "zone_id" {
+
+    default = "Z05454352LP47ZQWSG7WJ"
+}
+
+variable "domian_name" {
+
+      default = "dsecops88.online"
+}
+
+variable "list_fruits" {
+
+      default = ["banana","mango","banana","apple"]
+}
+
+variable "set_fruits" {
+
+      type = set(string)
+      default = ["banana","mango","banana","apple"]
+}
