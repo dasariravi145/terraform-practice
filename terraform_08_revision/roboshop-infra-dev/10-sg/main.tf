8f112c68c217c62ec87c3da50fe32bb7b33c97ee
unknown <dasariravid145@gmail.com>
2026-07-11 20:43:44 +0530

    terrraform

---
+++ terraform_08_revision/roboshop-infra-dev/10-sg/main.tf
@@ -0,0 +1,9 @@
+module "sg" {
+
+      source = "../../terraform-aws-sg"
+      project = var.project
+      environment = var.environment
+      sg_name = "mongodb"
+      vpc_id = local.vpc_id
+      
+}
